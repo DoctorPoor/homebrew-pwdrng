1211cacd94073b809e7b8818c1a78769be8d9c95
perry <8948878+DoctorPoor@users.noreply.github.com>
2021-11-27 23:52:25 -0800

    Updated to v.0.1.0

--- pwdrng.rb
+++ pwdrng.rb
@@ -1,8 +1,8 @@
 class Pwdrng < Formula
   desc "Simple command-line tool for generating passwords"
   homepage "https://github.com/DoctorPoor/pwdrng"
-  url "https://github.com/DoctorPoor/pwdrng/archive/refs/tags/v0.0.0.tar.gz"
-  sha256 "d506c038e1238a9ad603d94addf3428ebf26153ae7c8cb9e8eb6643f578fe6a2"
+  url "https://github.com/DoctorPoor/pwdrng/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "25f19fcf528de84ec6589d58372e7e3cef8332d2c052d68198477da59921ac21"
   license "MIT"
   head "https://github.com/DoctorPoor/pwdrng.git"
 
