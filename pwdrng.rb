df71fb1b6ecdc3be55591ada9d8cd8f74651ad7a
perry <8948878+DoctorPoor@users.noreply.github.com>
2021-11-27 19:26:06 -0800

    Added pwdrng.rb v0.0.0

---
+++ pwdrng.rb
@@ -0,0 +1,16 @@
+class Pwdrng < Formula
+  desc "Simple CLI for generating passwords"
+  homepage "https://github.com/DoctorPoor/pwdrng"
+  url "https://github.com/DoctorPoor/pwdrng/archive/refs/tags/v0.0.0.tar.gz"
+  sha256 "d506c038e1238a9ad603d94addf3428ebf26153ae7c8cb9e8eb6643f578fe6a2"
+  license "MIT"
+
+  def install
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+  end
+
+  test do
+    system "false"
+  end
+end
+
