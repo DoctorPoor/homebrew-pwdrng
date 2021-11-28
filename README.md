6297c6082ae5b24d1a2a599580544f0537db781e
perry <8948878+DoctorPoor@users.noreply.github.com>
2021-11-27 19:45:55 -0800

    Added README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# Homebrew pwdrng
+
+Homebrew formula for [pwdrng](https://github.com/DoctorPoor/pwdrng)
+
+## Installation and Usage
+
+```
+$ brew tap doctorpoor/pwdrng
+$ brew install pwdrng
+$ pwdrng
+```
+
--- pwdrng.rb
+++ pwdrng.rb
@@ -1,5 +1,5 @@
 class Pwdrng < Formula
-  desc "Simple CLI for generating passwords"
+  desc "Simple command-line tool for generating passwords"
   homepage "https://github.com/DoctorPoor/pwdrng"
   url "https://github.com/DoctorPoor/pwdrng/archive/refs/tags/v0.0.0.tar.gz"
   sha256 "d506c038e1238a9ad603d94addf3428ebf26153ae7c8cb9e8eb6643f578fe6a2"
