68851898911fb16fea4938a74c0f39393b76854d
perry <8948878+DoctorPoor@users.noreply.github.com>
2021-11-27 23:50:40 -0800

    Fixed formula install

--- pwdrng.rb
+++ pwdrng.rb
@@ -4,9 +4,12 @@ class Pwdrng < Formula
   url "https://github.com/DoctorPoor/pwdrng/archive/refs/tags/v0.0.0.tar.gz"
   sha256 "d506c038e1238a9ad603d94addf3428ebf26153ae7c8cb9e8eb6643f578fe6a2"
   license "MIT"
+  head "https://github.com/DoctorPoor/pwdrng.git"
+
+  depends_on "go" => :build
 
   def install
-    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "go", "build", "-o", bin/"pwdrng"
   end
 
   test do
